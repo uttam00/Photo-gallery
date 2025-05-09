012dadc3047838ef2672915f9f0c812c6ae424ff
Abhay-Deora <abhay@intellimedianetworks.net>
2025-05-09 18:55:36 +0530

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['mongodb'],
-  },
+  // experimental: {
+  //   serverComponentsExternalPackages: ['mongodb'],
+  // },
 }
 
 export default nextConfig
